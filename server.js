70a7248932fde573f88065904d56f34ec084cab1
cameron priestley <cameronpriestley@gmail.com>
2019-01-09 15:12:32 -0400

    third push

---
+++ server.js
@@ -0,0 +1,20 @@
+const https   = require('https');
+const express = require('express');
+const fs      = require('fs');
+const app     = express();
+
+
+var options = {
+  key: fs.readFileSync('./conf/key.pem'),
+  cert: fs.readFileSync('./conf/cert.pem')
+}
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>res.sendFile('index.html'));
+
+
+
+https.createServer(options, app).listen(3000, () => console.log("server is on"));
+
+//app.listen(3000, () =>console.log("server is on"));
